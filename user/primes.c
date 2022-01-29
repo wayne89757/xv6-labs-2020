e2339914b9ae543c9ee3ebe24b90cc63b22d347e
wayne <wwayne89757@gmail.com>
2022-01-29 16:50:51 +0800

    primes v2, more readable

--- user/primes.c
+++ user/primes.c
@@ -6,42 +6,41 @@ int
 main(int argc, char *argv[])
 {
   int ppl[2], ppr[2];
-  int pid;
-  int i, first, n;
+  int pid, first, i;
 
-  pipe(ppl);
-
+  pipe(ppr);
+  
   pid = fork();
-
-  while(pid == 0){
-    close(ppl[1]);
-    n = read(ppl[0], &first, 4);
-    if(n<=0) exit(0);
-    printf("prime %d\n", first);
-    pipe(ppr);
-    
-    pid = fork();
-    if(pid==0){
+  if(pid==0){
+     while(pid==0){
       ppl[0] = ppr[0];
-      ppl[1] = ppr[1];
-      continue;
-    }
-    close(ppr[0]);
-    while(read(ppl[0], &i, 4)){
-      if(i%first!=0)
-        write(ppr[1], &i, 4);
-    }
-    close(ppr[1]);
-    close(ppl[0]);
-    wait(0);
-    exit(0);
-  }
+      close(ppr[1]);
+      if(read(ppl[0], &first, 4)==0) break;
+      printf("prime %d\n", first);
+      
+      pipe(ppr);
+      pid = fork();
+      if(pid == 0){
+        continue;
+      }
+      else{
+        while(read(ppl[0], &i, 4))
+          if(i%first!=0)
+            write(ppr[1], &i, 4);
 
-  close(ppl[0]);
-  for(i=2;i<=35;i++)
-    write(ppl[1], &i, 4);
-  close(ppl[1]);
-  wait(0);
+        close(ppr[1]);
+        close(ppl[0]);
+        wait(0);
+      }
+     }
+  }
+  else{
+    close(ppr[0]);
+    for(int i=2;i<=35;i++)
+      write(ppr[1], &i, 4);
+    close(ppr[1]);
+    wait(0);
+  }
 
   exit(0);
 }
