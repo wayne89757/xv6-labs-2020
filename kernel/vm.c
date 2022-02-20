30c6eb135085c81d08bcf88e715a054fc5affe9d
wayne <wwayne89757@gmail.com>
2022-02-20 23:59:54 +0800

    pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+//print pagetable information
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+
+  for(int i = 0; i < 512; i++) {
+    pte_t pd = pagetable[i];
+    if(pd & PTE_V) {
+      pagetable_t pt = (void *)PTE2PA(pd);
+      printf("..%d: pte %p pa %p\n", i, pd, pt);
+      for(int j = 0; j < 512; j++) {
+        pte_t pd2 = pt[j];
+        if(pd2 & PTE_V) {
+          pagetable_t pt2 = (void *)PTE2PA(pd2);
+          printf(".. ..%d: pte %p pa %p\n", j, pd2, pt2);
+          for(int k = 0; k < 512; k++) {
+            pte_t pte = pt2[k];
+            if(pte & PTE_V) {
+              printf(".. .. ..%d: pte %p pa %p\n", k, pte, PTE2PA(pte));
+            }
+          }
+        }
+      }
+    }
+  }
+}
