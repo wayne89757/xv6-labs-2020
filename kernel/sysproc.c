739cb1ed7a0a5f1d9e6dbc3c232703242e494a91
wayne <wwayne89757@gmail.com>
2022-03-04 14:55:00 +0800

    lazy allocation

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -43,12 +43,15 @@ sys_sbrk(void)
 {
   int addr;
   int n;
+  struct proc *p = myproc();
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  if(growproc(n) < 0)
+  addr = p->sz;
+  if(addr + n < 0)
     return -1;
+  p->sz += n;
+
   return addr;
 }
 
--- kernel/trap.c
+++ kernel/trap.c
@@ -67,6 +67,20 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
+  } else if((r_scause() == 13 || r_scause() == 15) && r_stval() < p->sz) {
+    uint64 va, pa;
+    va = r_stval();
+    pa = (uint64)kalloc();
+    if(pa == 0){
+      p->killed = 1;
+    } else {
+      memset((void *)pa, 0, PGSIZE);
+      va = PGROUNDDOWN(va);
+      if(mappages(p->pagetable, va, PGSIZE, pa, PTE_U|PTE_W|PTE_R) != 0) {
+        kfree((void *)pa);
+        p->killed = 1;
+      }
+    }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
