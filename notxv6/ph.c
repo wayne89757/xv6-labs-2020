b4d41968b54574e9a71864eb178789940bb057c8
wayne <wwayne89757@gmail.com>
2022-03-09 21:04:00 +0800

    using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -14,6 +14,7 @@ struct entry {
   struct entry *next;
 };
 struct entry *table[NBUCKET];
+pthread_mutex_t locks[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
@@ -42,6 +43,8 @@ void put(int key, int value)
 
   // is the key already present?
   struct entry *e = 0;
+  // must lock before looking into table
+  pthread_mutex_lock(&locks[i]);
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
@@ -53,6 +56,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -114,6 +118,10 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+
+  // initial all locks
+  for(int i = 0; i < NBUCKET; i++)
+    pthread_mutex_init(&locks[i], NULL);
 
   //
   // first the puts
