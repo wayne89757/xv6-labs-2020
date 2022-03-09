e1a331f978fe6994a7ba70e21b3d4dc67b87daf8
wayne <wwayne89757@gmail.com>
2022-03-09 22:24:06 +0800

    Barrier

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,28 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread ++;
+  while(round == bstate.round) {
+    if(bstate.nthread == nthread) {
+      bstate.round ++;
+      break;
+    }
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_cond_broadcast(&bstate.barrier_cond);
+
+  bstate.nthread --;
+  while(round != bstate.round) {
+    if(bstate.nthread == 0) {
+      round = bstate.round;
+      break;
+    }
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  pthread_cond_broadcast(&bstate.barrier_cond);
+
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
