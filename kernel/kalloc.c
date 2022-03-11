15bf3256311bab7b3e616950b7ee268800f301f0
wayne <wwayne89757@gmail.com>
2022-03-11 23:03:42 +0800

    kmem lock per cpu, could cause deadlock

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void _kinit();
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -18,15 +19,21 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct kmem {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmems[NCPU];
 
+// note that kinit() is exceuted in main in cpu0
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  struct kmem * kmem;
+
+  // hard code here, since I don't know how to get
+  // number of cores dynamicly.
+  for(kmem = kmems; kmem < kmems + 3; kmem++) 
+    initlock(&kmem->lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -47,6 +54,7 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  struct kmem * kmem;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -55,28 +63,92 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
+  
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+
+  kmem = kmems + cpuid();
+
+  acquire(&kmem->lock);
+  r->next = kmem->freelist;
+  kmem->freelist = r;
+  release(&kmem->lock);
+
+  pop_off();
 }
 
-// Allocate one 4096-byte page of physical memory.
-// Returns a pointer that the kernel can use.
-// Returns 0 if the memory cannot be allocated.
+// Must be called with interrupts disabled,
+// kmems[cpuid()].lock must be hold.
+void *
+steal(void)
+{ 
+  struct run *r = 0, *slow, *fast;
+  struct kmem * kmem;
+  int hartid = cpuid();
+
+  if(intr_get())
+    panic("steal(): interrupt enable");
+
+  for(int i = 0; i < 3; i++) {
+    // Avoid acquiring a lock that is already hold.
+    if(i == hartid)
+      continue;
+    kmem = kmems + i;
+
+    acquire(&kmem->lock);
+
+    r = kmem->freelist;
+    // bug: fucking stupid
+    // if(r == 0) continue;
+    if(!r){
+      release(&kmem->lock);
+      continue;
+    }
+    slow = r;
+    fast = slow->next;
+
+    while(fast){
+      fast = fast->next;
+      if(fast)
+        fast = fast->next;
+      slow = slow->next;
+    }
+    
+    kmem->freelist = slow->next;
+    slow->next = 0;
+
+    release(&kmem->lock);
+
+    return r;
+  }
+  
+  if(r)
+    panic("steal");
+  return r;
+}
+
 void *
 kalloc(void)
 {
-  struct run *r;
+  struct run *r = 0;
+  struct kmem * kmem;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+
+  kmem = kmems + cpuid();
+
+  acquire(&kmem->lock);
+
+  r = kmem->freelist;
+  if(!r)
+    r = steal();
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem->freelist = r->next;
+
+  release(&kmem->lock);
+  pop_off();
 
   if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+    memset((char*)r, 5, PGSIZE);
   return (void*)r;
 }
